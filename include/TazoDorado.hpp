13bda846bd66f890d6a8cddb515f751de6cfd46e
XxjemdxX <A23310348@live.ceti.mx>
2024-10-22 01:59:02 +0000

    clase 4

--- include/TazoDorado.hpp
+++ include/TazoDorado.hpp
@@ -6,8 +6,12 @@ public:
     TazoDorado() {
         this->flotando = false;
     }
-    ~TazoDorado() {}
-    void Flotar(){
+    ~TazoDorado(){}
+        void Flotar(){   
         this -> flotando = true;
+    }
+
+    bool Flotando(){
+        return this ->flotando;
     }
 };
